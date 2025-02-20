2125cb1e6659bcea26689a1c3c03f8d49e2d4e4e
Fred Lee <ftl2110@columbia.edu>
2025-02-20 18:10:52 -0500

    small update. Will merge this code into app.py

--- group_and_list.py
+++ group_and_list.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def load_data():
     '''
@@ -6,17 +7,22 @@ def load_data():
     formats columns for later use
     adds column names for empty values
     '''
+    
+    # absolute path
+    file_path = os.path.join(os.getcwd(), "un_data.csv")
 
-    data = pd.read_csv(r'fred_angel_project\un_data.csv', 
+    data = pd.read_csv(file_path, 
                        encoding="ISO-8859-1", skiprows = 1, header = 0)
     
 
     
     data = data.rename(columns={"Region/Country/Area": "Region Code","Unnamed: 1": "Region"})
     
-    #Optional validation
+    return data
+    
+    #Validation
     print(data.columns)
-    print(data)
+    print(data.head(100))
 
 
 load_data()
